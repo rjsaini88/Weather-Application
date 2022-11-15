eeb81cd70dd7b00b09309e161de9152acac553c6
Rj Saini <rsaini88@gmail.com>
2022-11-15 00:34:03 -0800

    Added from HW tutorial to display weather

--- script.js
+++ script.js
@@ -1,94 +1,66 @@
+var input = document.querySelector("input")
+input.addEventListener('keyup', function(event){
+  if (event.key ==="Enter"){
+    createWeatherDisplay(event.target.value)
+  }
+})
 var API_key = "d7e0beaf6048af6805ba0b615e62bf01";
 //This function returns a promise which is fetch to Geocoding API to grab the location. Query is location and limit is 5 to fetch only 5 location based on query
-function getGeoLocation(query, limit=5){
-    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_key}`)
+function getGeoLocation(query, limit =5) {     //put this as a function so we can call this anytime query is the location passed in the function, 5 is the limit of results
+  return fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_key}`
+  );
 }
 
-function getCurrentWeather(arguments){
-    return fetch (`https://api.openweathermap.org/data/3.0/onecall?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial'}&appid=${API_key}`)
+function getCurrentWeather(arguments) {   //arguments are lat and lon  where the retured value of the of lat and lon passed into the function
+  return fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${arguments.lat}&lon=${
+      arguments.lon
+    }&units=${"imperial"}&lang=${"en"}&appid=${API_key}`
+  );
 }
 
-getGeoLocation("Riverside") //Get location, which is Riverside
-.then(response => response.json()) //then the response from api is parsed to JavaScript object
-.then(data => {                    //then the data received can be used to 
-    var{ lat, lon} = data[0]       //data is a array, setting data to index 0 to grab the first lat and lon from the array
-    getCurrentWeather({lat, lon})  //function to grab current weather based on 
-    .then(weatherResponse => weatherResponse.json())
-    .then(weatherData =>{
-        document.body.textContent = JSON.stringify(weatherData, null, 2)
-    })
-    .catch(error => {     //catch any error
-        document.body.textContent = error.message    //print the error in the body
-})
-})
-.catch(error => {
-    document.body.textContent = error.message
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getGeoLocation(query, limit = 5) {
-//   return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_key}`
-//   );
-// }
-
-// fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit={limit}&appid=${API_key}`)
-// function getCurrentWeather(arguments) {
-//   return fetch(
-//     `https://api.openweathermap.org/data/3.0/onecall?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial'}&appid=${API_key}`
-//   );
-// }
-
-
-// getGeoLocation("Long Beach")
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
-//       .then((weatherResonse) => weatherResonse.json())
-//       .then((weatherData) => {
-//         document.body.textContent = JSON.stringify(weatherData, null, 2);
-//       })
-//       .catch((error) => {
-//         document.body.textContent = error.message;
-//       });
-//   })
-//   .catch((error) => {
-//     document.body.textContent = error.message;
-//   });
-
-
-
-/*
-// getGeoLocation("Long Beach")
-//   .then(response => response.json())
-//   .then(data => {
-//     var {lat, lon} = data [0]
-//     console.log(data)
-//     getCurrentWeather({lat, lon})
-//     .then(weatherResonse => weatherResonse.json())
-//     .then(weatherData => {
-
-//         document.body.textContent = JSON.stringify(weatherData, null, 2);
-//     })
-//     .catch (error =>{
-//         document.body.textContent = error.message
-//   })
-// })
-//   .catch(error => {
-//     document.body.textContent = error.message;
-//   })
-
-*/
+function createWeatherDisplay (location){
+  return getGeoLocation(location) //Get location, which is Riverside, a function that retruns a promise, which is a fetch to the api geolocation
+  .then((response) => response.json()) //then the response from api is parsed to JavaScript object
+  
+  .then(data => {
+    if(data.length === 0){
+      var errorEl = document.createElement ('p')
+      errorEl.textContent = `We could not find ${location}`
+      document.body.appendChild(errorEl)
+    } else {
+      // var { lat, lon } = data[0]; //data is a array, setting data to index 0 to grab the first lat and lon from the results of getGeoLocation ("Riversive") funciton. 
+      // console.log(data[0].lat)
+      console.log(data)
+      //document.body.textContent = JSON.stringify(data, null, 2)   //stringify the data to convert the object into a string. 
+      //then the data received can be used to
+      getCurrentWeather({lat:data[0].lat,lon:data[0].lon}) //the argument lat and lon with data [0] passed into the getCurrentWeather function. 
+      //getCurrentWeather({ lat, lon }) //function to grab current weather based on lat and the lon we determined
+      .then((weatherResponse) => weatherResponse.json())  //parse the response received from getCurrentWeather  the wriiten function same as writing .then (function(weatherResponse){weatehrResonse.json()})
+   
+      .then(weatherData => {
+        var weatherPicture = document.createElement ('img')
+        weatherPicture.src = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`
+        var currentWeatehrStatement = document.createElement ('p')
+        currentWeatehrStatement.textContent = `The current weather is ${weatherData.current.weather[0].main}, with ${weatherData.current.weather[0].description}, and temperature of ${weatherData.current.temp}`
+          // document.body.textContent = JSON.stringify(weatherData, null, 2);  //stringfy data and print the data to screen. 
+          console.log(weatherData)
+          console.log(weatherData.current.weather[0].main)
+          console.log(weatherData.current.weather[0].description)
+          console.log(weatherData.current.temp)
+          document.body.appendChild(weatherPicture)
+          document.body.appendChild(currentWeatehrStatement)
+  
+      })
+        .catch((error) => {
+          //catch any error
+          document.body.textContent = error.message; //print the error in the body
+        });
+    }
+    
+  })
+  .catch((error) => {
+    document.body.textContent = error.message;
+  })
+}
