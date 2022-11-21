994e077ca7f9aac76a01c9ba0ebebf934bbd893a
Rj Saini <rsaini88@gmail.com>
2022-11-20 19:13:12 -0800

    Worked on localStorage

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+// window.onload = function (){
+//   if(window.jQuery){
+//     alert("yeah")
+//   }
+// }
+//
 var input = document.querySelector("input");
 var timeEl = $(".date");
 var currentTime = moment().format("dddd MMMM Do [at] LT");
-console.log(currentTime)
-timeEl.text(currentTime)
+console.log(currentTime);
+timeEl.text(currentTime);
 input.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
+    // location.reload();
+    // document.body.innerHTML=''
+    // clearPage(); //this needs corredction.
     createWeatherDisplay(event.target.value);
-    addToHistory(event.target.value);
-   displayHistory(); 
+    // addToHistory(event.target.value);
+// addToHistory();
+    // displayHistory();
   }
 });
 
@@ -16,24 +25,76 @@ input.addEventListener("keyup", function (event) {
 
 // }
 
-function displayHistory(){
-  var previousSearchHistory = localStorage.getItem("history");
-if (previousSearchHistory) {
-  previousSearchHistory = JSON.parse(previousSearchHistory);
-} else {
-  previousSearchHistory = [];
+// function displayHistory() {
+//   var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+//   // var previousSearchHistory = localStorage.getItem("history");
+//   // if (previousSearchHistory) {
+//   //   previousSearchHistory = JSON.parse(previousSearchHistory);
+//   // } else {
+//   //   previousSearchHistory = [];
+//   // }
+
+//   for (var i = 0; i < searchHistory.length; i++) {
+//     var historybtn = document.createElement("button");
+//     var historyItem = previousSearchHistory[i];
+//     historybtn.textContent = historyItem;
+//     historybtn.addEventListener("click", function (event) {
+//       createWeatherDisplay(event.target.textContent);
+//     });
+//     $(".history").append(historybtn);
+//     //     document.body.appendChild(historybtn);
+//     //     $('.weather').text(currentWeatherStatement)
+//     // $('#img').append(image)
+//   }
+// }
+
+function addToHistory(location) {
+  var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+  console.log(searchHistory);
+  // for (var i=0; i<searchHistory.length;i++){
+  //   if(searchHistory === location){
+  //     return;
+  //   }
+  //   searchHistory.push(location);
+  //   localStorage.setItem('history', JSON.stringify(searchHistory));
+  // }
+
+  for (var i = 0; i < searchHistory.length; i++) {
+    // <------> Question about localStorage
+if(searchHistory[i] === location){
+    // if (searchHistory.includes(location)) {
+      return;
+    }
+    // }
+    //   searchHistory.push(location);
+    //   localStorage.setItem("history", JSON.stringify(searchHistory));
+
+  }
+    // else {
+      searchHistory.push(location);
+    // searchHistory = [location];
+    localStorage.setItem("history", JSON.stringify(searchHistory));
+    // }
 }
 
-for (var i = 0; i < previousSearchHistory.length; i++) {
-  var historybtn = document.createElement("button");
-  var historyItem = previousSearchHistory[i];
-  historybtn.textContent = historyItem;
-  historybtn.addEventListener("click", function (event) {
-    createWeatherDisplay(event.target.textContent);
-  });
-  document.body.appendChild(historybtn);
-}
-}
+// }
+// var searchHistory = localStorage.getItem("history");
+// if (searchHistory) {
+//   searchHistory = JSON.parse(searchHistory);
+//   if (searchHistory.includes(location)) {
+//     return;
+//   }
+
+//   searchHistory.push(location);
+//   localStorage.setItem("history", JSON.stringify(searchHistory));
+// } else {
+//   searchHistory = [location];
+//   localStorage.setItem("history", JSON.stringify(searchHistory));
+// }
+// }}
+// function clearPage(){
+//   document.body.innerHTML = ''
+// }
 var API_key = "d7e0beaf6048af6805ba0b615e62bf01";
 //This function returns a promise which is fetch to Geocoding API to grab the location. Query is location and limit is 5 to fetch only 5 location based on query
 function getGeoLocation(query, limit = 5) {
@@ -51,32 +112,14 @@ function getCurrentWeather(arguments) {
   );
 }
 
-function addToHistory(location) {
-  // for (var i = 0; i < searchHistory; i++) { . <------> Question about localStorage
-  //   if (searchHistory.includes(location)) {
-  //     return;
-  //   }
-  //   searchHistory.push(location);
-  //   localStorage.setItem("history", JSON.stringify(searchHistory));
-  // }
+// clearPage () => {document.body.innerHTML=''}
 
-  var searchHistory = localStorage.getItem("history");
-  if (searchHistory) {
-    searchHistory = JSON.parse(searchHistory);
-    if (searchHistory.includes(location)) {
-      return
-    }
-
-    searchHistory.push(location);
-    localStorage.setItem("history", JSON.stringify(searchHistory));
-  } else {
-    searchHistory = [location];
-    localStorage.setItem("history", JSON.stringify(searchHistory));
-  }
-}
+$(".error").hide();
 
 function createWeatherDisplay(location) {
+  // clearPage()
   // addToHistory(location);
+  //create function to remove inner HTML ---->
   return getGeoLocation(location) //Get location, which is Riverside, a function that retruns a promise, which is a fetch to the api geolocation
     .then((response) => response.json()) //then the response from api is parsed to JavaScript object
 
@@ -84,7 +127,12 @@ function createWeatherDisplay(location) {
       if (data.length === 0) {
         var errorEl = document.createElement("p");
         errorEl.textContent = `We could not find ${location}`;
-        document.body.appendChild(errorEl);
+        $(".error").show();
+        setTimeout(function () {
+          $(".error").hide();
+        }, 3000);
+        // $('.error').append(errorEl)
+        // document.body.appendChild(errorEl);
       } else {
         // var { lat, lon } = data[0]; //data is a array, setting data to index 0 to grab the first lat and lon from the results of getGeoLocation ("Riversive") funciton.
         // console.log(data[0].lat)
@@ -96,18 +144,33 @@ function createWeatherDisplay(location) {
           .then((weatherResponse) => weatherResponse.json()) //parse the response received from getCurrentWeather  the wriiten function same as writing .then (function(weatherResponse){weatehrResonse.json()})
 
           .then((weatherData) => {
-            var weatherPicture = document.createElement("img");
-            weatherPicture.src = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`;
-            var currentWeatehrStatement = document.createElement("p");
-            currentWeatehrStatement.textContent = `The current weather is ${weatherData.current.weather[0].main}, with ${weatherData.current.weather[0].description}, and temperature of ${weatherData.current.temp}`;
+            // var weatherPicture = document.createElement("img");
+            var weatherPicture = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`;
+            var image = new Image();
+            image.src = weatherPicture;
+            // var currentWeatherStatement = document.createElement("p");
+            var currentWeatherStatement = `The current weather is ${weatherData.current.weather[0].main}, with ${weatherData.current.weather[0].description}, and temperature of ${weatherData.current.temp} degrees`;
+            console.log(currentWeatherStatement);
             // document.body.textContent = JSON.stringify(weatherData, null, 2);  //stringfy data and print the data to screen.
             console.log(weatherData);
             console.log(weatherData.current.weather[0].main);
             console.log(weatherData.current.weather[0].description);
             console.log(weatherData.current.temp);
-            document.body.appendChild(weatherPicture);
-            document.body.appendChild(currentWeatehrStatement);
-          
+            $(".weather").html(currentWeatherStatement);
+            // $('#img').append(image)
+            // const weatherStatement = document.querySelector(".weather");
+            // const pic = document.querySelector('img');
+            // weatherStatement.textContent = currentWeatherStatement;
+            // document.querySelector('.weather').appendChild (currentWeatherStatement);
+            // const weather = document.querySelector(".weather");
+            // weather.textContent = currentWeatherStatement;
+            // document.querySelector('img').innerHTML = weatherPicture;
+            // $('.img').append(weatherPicture);
+            // $('.weather').appendChild(currentWeatherStatement);
+            // document.body.appendChild(weatherPicture);
+            // document.getElementById('#img').appendChild(weatherPicture);
+
+            // document.body.appendChild(currentWeatherStatement);
           })
           .catch((error) => {
             //catch any error
@@ -118,4 +181,12 @@ function createWeatherDisplay(location) {
     .catch((error) => {
       document.body.textContent = error.message;
     });
+}
+
+(clearPage) => {
+  $(".cards").remove();
+};
+
+function viewHistory() {
+  let history;
 }
